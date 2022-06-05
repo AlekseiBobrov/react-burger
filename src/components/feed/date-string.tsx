e6dfd84511463d3d122b552a67e33755c330df5a
Алексей Бобров <alexeybobrov@gmail.com>
2022-06-05 10:34:21 +0300

    HotFix DateString

--- src/components/feed/date-string.tsx
+++ src/components/feed/date-string.tsx
@@ -8,8 +8,12 @@ interface IDateStringProps{
 const DateString: FC<IDateStringProps> = ({sdate}) => {
     const date = new Date(sdate);
     const time = `${addZero(date.getHours())}:${addZero(date.getMinutes())}`
-    const offset = (new Date().setHours(0, 0, 0, 0) - date.setHours(0, 0, 0, 0))/86400000;
-    const stringOffset = offset <= 1?"Сегодня":offset <= 2?"Вчера":`${offset} дня назад`;
+
+    const today =  new Date();
+    today.setHours(0, 0, 0, 0)
+    date.setHours(0, 0, 0, 0)
+    const offset = (today.getTime() - date.getTime())/86400000;
+    const stringOffset = offset < 1?"Сегодня":offset < 2?"Вчера":`${offset} дня назад`;
 
     return (
         <p className="text text_type_main-default text_color_inactive">
